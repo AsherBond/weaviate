9c674733dcb57621f726e44ca51a176a00439164
Dirk Kulawiak <dirk@semi.technology>
2026-04-09 12:43:19 -0700

    Remove double comment

--- adapters/repos/db/lsmkv/fadvise_other.go
+++ adapters/repos/db/lsmkv/fadvise_other.go
@@ -11,17 +11,6 @@
 
 //go:build !linux
 
-//                           _       _
-// __      _____  __ ___   ___  __ _| |_ ___
-// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
-//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
-//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
-//
-//  Copyright © 2016 - 2026 Weaviate B.V. All rights reserved.
-//
-//  CONTACT: hello@weaviate.io
-//
-
 package lsmkv
 
 import "os"
